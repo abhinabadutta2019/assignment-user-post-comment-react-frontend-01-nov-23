f42cbdf26611b03c49957d3a0fa8ef4c5aa87f2c
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-04 20:25:18 +0530

    before changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,23 +6,26 @@ import { Post } from "../components/Post";
 function Home() {
   const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    async function fetchPosts() {
-      try {
-        const response = await fetch("http://localhost:3006/posts");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        //
-        console.log(data, "data");
-        //
-        setPosts(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
+  //
+  async function fetchPosts() {
+    try {
+      const response = await fetch("http://localhost:3006/posts");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
+      const data = await response.json();
+      //
+      console.log(data, "data");
+      //
+      setPosts(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  }
 
+  //
+
+  useEffect(() => {
     fetchPosts();
   }, []);
 
