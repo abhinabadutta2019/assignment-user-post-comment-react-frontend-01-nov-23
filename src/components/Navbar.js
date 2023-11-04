da8a2c336c65b1fd27530e3f05a0e02165aaf153
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-04 18:36:38 +0530

    added logout button and its function in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  //
+  const { logout, user } = useContext(AuthContext);
+  //
+  const logoutHandler = () => {
+    //
+    logout();
+  };
+  //
   return (
     <header>
       <Link to="/">
         <h1>My App Title</h1>
       </Link>
       <Link to="/auth">Auth</Link>
+      <button onClick={logoutHandler}>Logout</button>
     </header>
   );
 };
