5f095c8f2ceea621e0ab701a073bea5a749a7d84
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-04 20:56:34 +0530

    add post Navbar button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
@@ -6,9 +6,15 @@ const Navbar = () => {
   //
   const { logout, user } = useContext(AuthContext);
   //
+  const navigate = useNavigate();
+  //
   const logoutHandler = () => {
     //
     logout();
+  };
+  //
+  const goToCreatePage = () => {
+    navigate("/create"); // Use the navigate function to go to the "/create" page
   };
   //
   return (
@@ -17,8 +23,8 @@ const Navbar = () => {
         <h1>My App Title</h1>
       </Link>
       <Link to="/auth">Auth</Link>
-      <Link to="/create">Create</Link>
-
+      {/* <Link to="/create">Create</Link> */}
+      <button onClick={goToCreatePage}>Create Post</button>
       <button onClick={logoutHandler}>Logout</button>
     </header>
   );
