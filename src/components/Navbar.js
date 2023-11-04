ad0b75f441ca6a89b58da0fb455ee9e2cf514f57
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-04 21:59:00 +0530

    bootstrap added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,41 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 const Navbar = () => {
-  //
   const { logout, user } = useContext(AuthContext);
-  //
   const navigate = useNavigate();
-  //
+
   const logoutHandler = () => {
-    //
     logout();
   };
-  //
+
   const goToCreatePage = () => {
-    navigate("/create"); // Use the navigate function to go to the "/create" page
+    navigate("/create");
   };
-  //
+
   return (
-    <header>
-      <Link to="/">
-        <h1>My App Title</h1>
+    <header className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/" className="navbar-brand">
+        My App Title
       </Link>
-      {/* <Link to="/auth">Auth</Link> */}
-      {/* <Link to="/create">Create</Link> */}
 
       {user && (
-        <>
-          <button onClick={goToCreatePage}>Create Post</button>
-          <button onClick={logoutHandler}>Logout</button>
-        </>
+        <div className="navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <button className="btn btn-primary" onClick={goToCreatePage}>
+                Create Post
+              </button>
+            </li>
+            <li className="nav-item">
+              <button className="btn btn-danger" onClick={logoutHandler}>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
       )}
     </header>
   );
