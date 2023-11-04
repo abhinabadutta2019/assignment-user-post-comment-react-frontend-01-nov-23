4d66a591556a38df2869a159e020c385fd4e138c
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-04 16:04:58 +0530

    made changes in post.js

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 function Post({ post }) {
+  //
+  const addCommentHandler = () => {};
+  //
   return (
     <div key={post._id}>
       <h2>Post: {post.content}</h2>
       <h3>Comments:</h3>
+      {/*  */}
+      <button onClick={addCommentHandler}>Add comments</button>
       <ul>
         {post.comments.map((comment, index) => (
           //
