29bbdb5f298511afa27eca9c2a989e4df0e4aa69
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-04 22:10:11 +0530

    added bootstrap to Auth.js

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -3,6 +3,7 @@ import { AuthContext } from "../context/AuthContext";
 
 const Auth = () => {
   const { login } = useContext(AuthContext);
+  const [showRegistration, setShowRegistration] = useState(false);
 
   const loginHandler = async (event) => {
     event.preventDefault();
@@ -73,25 +74,84 @@ const Auth = () => {
   };
 
   return (
-    <div>
-      <h1>Auth</h1>
-      <h2>Registration form</h2>
-      <form onSubmit={registerHandler}>
-        <label>Username</label>
-        <input id="registerUsername" type="text" required />
-        <label>Password</label>
-        <input id="registerPassword" type="text" required />
-        <button type="submit">Register</button>
-      </form>
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Authentication</h1>
+      <div className="d-flex justify-content-center">
+        <button
+          className="btn btn-primary mb-3"
+          onClick={() => setShowRegistration(!showRegistration)}
+        >
+          {showRegistration ? "Switch to Login" : "Switch to Registration"}
+        </button>
+      </div>
 
-      <h2>Login form</h2>
-      <form onSubmit={loginHandler}>
-        <label>Username</label>
-        <input id="loginUsername" type="text" required />
-        <label>Password</label>
-        <input id="loginPassword" type="text" required />
-        <button type="submit">Login</button>
-      </form>
+      {showRegistration ? (
+        <div className="d-flex justify-content-center">
+          <div className="col-6">
+            <h2 className="mb-3">Registration Form</h2>
+            <form onSubmit={registerHandler}>
+              <div className="mb-3">
+                <label htmlFor="registerUsername" className="form-label">
+                  Username
+                </label>
+                <input
+                  id="registerUsername"
+                  type="text"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="registerPassword" className="form-label">
+                  Password
+                </label>
+                <input
+                  id="registerPassword"
+                  type="password"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-success">
+                Register
+              </button>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div className="d-flex justify-content-center">
+          <div className="col-6">
+            <h2 className="mb-3">Login Form</h2>
+            <form onSubmit={loginHandler}>
+              <div className="mb-3">
+                <label htmlFor="loginUsername" className="form-label">
+                  Username
+                </label>
+                <input
+                  id="loginUsername"
+                  type="text"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="loginPassword" className="form-label">
+                  Password
+                </label>
+                <input
+                  id="loginPassword"
+                  type="password"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
