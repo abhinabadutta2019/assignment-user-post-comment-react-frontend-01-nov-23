962c875468a7ecab94cefb72bb292c6315c6dcb7
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-04 21:14:28 +0530

    route conditions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
 import { Auth } from "./pages/Auth";
 import { CreateForm } from "./pages/CreateForm";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  //
+  const { user } = useContext(AuthContext);
+  //
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/create" element={<CreateForm />} />
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/auth" />}
+            />
+            {/* <Route path="/auth" element={<Auth />} /> */}
+            <Route
+              path="/auth"
+              element={!user ? <Auth /> : <Navigate to="/" />}
+            />
+            {/* <Route path="/create" element={<CreateForm />} /> */}
+            <Route
+              path="/create"
+              element={user ? <CreateForm /> : <Navigate to="/auth" />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
